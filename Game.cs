d322d6895f24489ca2e788c3492a7c307ab60091
Mikkel1997 <158566210+Mikkel1997@users.noreply.github.com>
2024-04-11 13:12:26 +0200

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_3___Genspil
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public int Age { get; set; }
+        public int Players { get; set; }
+        public string Condition { get; set; }
+        public decimal Price { get; set; }
+    }
+}
